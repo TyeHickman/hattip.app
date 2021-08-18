27b86134796103855bcf177eed20712b86f45f7a
Tye <tye.hickman89@gmail.com>
2021-08-18 00:12:47 -0500

    Adding regiter workflow items

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from  "@angular/router";
 import { AuthService } from '../auth.service';
+import { Auth } from 'aws-amplify';
 
 
 @Component({
@@ -10,14 +11,42 @@ import { AuthService } from '../auth.service';
 })
 export class RegisterPage implements OnInit {
 
+
+  username: string;
+  password: string;
+  email: string;
+
   constructor(private  authService:  AuthService, private  router:  Router) { }
 
   ngOnInit() {
   }
 
-  register(form) {
-    this.authService.register(form.value).subscribe((res) => {
-      this.router.navigateByUrl('home');
-    });
+  async register(form) {
+    
+    console.log(form.value);
+
+    //TODO: uncomment for real user registration
+    // try {
+    //   const { user } = await Auth.signUp({
+    //       username : form.value.username,
+    //       password : form.value.password,
+    //       attributes: {
+    //           email : form.value.email 
+    //       }
+    //   });
+    //   console.log(user);
+    // } catch (error) {
+    //   console.log('error signing up:', error);
+    // }
   }
+
+  openConfirm(){
+    console.log('Confirm Registration...');
+    let confirmArea = document.getElementById("confirmation");
+    let confirmInput = document.createElement('ion-input')
+    confirmArea.appendChild(confirmInput)
+  }
+
+
 }
+
